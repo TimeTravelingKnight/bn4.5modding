2e891dbf1c9067575d01116cd6f3cc24bb69d978
Pulok Rahman <timetravelingknight@gmail.com>
2024-11-26 18:19:59 -0800

    updated sprites

--- SettingLasers/HeadSummon.asm
+++ SettingLasers/HeadSummon.asm
@@ -21,10 +21,11 @@ strb r0,[r5,0x10]
 mov r4,0x1
 lsl r4,r4,0x8
 ;add r4,1
-mov r1,0x23
+;mov r1,0x23
 BXwithR11 navistatus3|1
-add r0,r0,r0
-sub r0,1
+;add r0,r0,r0
+;sub r0,1
+mov r4,48
 add r4,r4,r0
 lsl r4,r4,0x8
 add r4,0x2E

a8de12b20a2032c2a76c14de55ee7415c3596c50
Pulok Rahman <timetravelingknight@gmail.com>
2024-11-26 16:53:22 -0800

    Cleaned final part of timestop

--- SettingLasers/LaserDmg.asm
+++ SettingLasers/LaserDmg.asm
@@ -2,12 +2,25 @@ Shl03EV equ 0x8094BAE
 SetLaserDmg:
 push r14
 BXwithR11 Shl03EV|1
+
+ldrb r1,[r5]
+mov r2,0x10
+AND r1,r2
+tst r1,r1
+bne @@PutCounterForDmg
+ldrb r1,[r0]
+mov r2,0x10
+eor r1,r2
+strb r1,[r0]
+
+ 
+@@PutCounterForDmg:
 push r5
 mov r5,r0
 mov r0,45
 strh r0,[r5,0x20]
-
 pop r5
+
 
 pop r15
 .pool
